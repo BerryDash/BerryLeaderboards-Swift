d8329bf6ed36b3125cb603513d54f5fe6262da05
Lncvrt <lncvrtreal@gmail.com>
2025-06-09 16:08:13 -0700

    Initial Commit

---
+++ BerryLeaderboards/BerryLeaderboardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerryLeaderboardsApp.swift
+//  BerryLeaderboards
+//
+//  Created by Lncvrt on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct BerryLeaderboardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BerryLeaderboardsTests/BerryLeaderboardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BerryLeaderboardsTests.swift
+//  BerryLeaderboardsTests
+//
+//  Created by Lncvrt on 6/9/25.
+//
+
+import Testing
+@testable import BerryLeaderboards
+
+struct BerryLeaderboardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
